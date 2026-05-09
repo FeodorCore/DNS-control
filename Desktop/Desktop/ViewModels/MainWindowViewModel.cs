c9183f00023dc5f5254d2166851e9e43b91d4a7e
FeodorCore <tntgmgm@gmail.com>
2026-05-09 19:44:18 +0300

    mvvm main correction

--- Desktop/Desktop/ViewModels/MainWindowViewModel.cs
+++ Desktop/Desktop/ViewModels/MainWindowViewModel.cs
@@ -7,8 +7,12 @@ namespace Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private ViewModelBase? _currentView;
+    [ObservableProperty] private ViewModelBase? _currentView;
+
+    [ObservableProperty] private NavigationItem? selectedMenuItem;
+
+    [ObservableProperty] private string _currentTitle = "Главная";
+
 
     public ObservableCollection<NavigationItem> MenuItems { get; } = new()
     {
@@ -20,11 +24,22 @@ public partial class MainWindowViewModel : ViewModelBase
         new NavigationItem("Отчёты", typeof(ReportsViewModel)),
     };
 
-    [RelayCommand]
-    private void Navigate(NavigationItem? item)
+    public MainWindowViewModel()
     {
-        if (item?.ViewModelType is not null)
-            CurrentView = (ViewModelBase)Activator.CreateInstance(item.ViewModelType)!;
+        if (MenuItems.Count > 0) SelectedMenuItem = MenuItems[0];
+    }
+
+    partial void OnSelectedMenuItemChanged(NavigationItem? value)
+    {
+        if (value is not null)
+        {
+            CurrentTitle = value.Title;
+            
+            if (value.ViewModelType is not null)
+            {
+                CurrentView = (ViewModelBase)Activator.CreateInstance(value.ViewModelType)!;
+            }
+        }
     }
 }
 
--- Desktop/Desktop/Views/MainWindow.axaml.cs
+++ Desktop/Desktop/Views/MainWindow.axaml.cs
@@ -1,22 +1,11 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Desktop.ViewModels;
 
 namespace Desktop.Views;
+
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
-        menuList.SelectionChanged += OnMenuSelectionChanged;
-    }
-
-    private void OnMenuSelectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        if (DataContext is MainWindowViewModel vm && menuList.SelectedItem is NavigationItem navItem)
-        {
-            vm.NavigateCommand.Execute(navItem);
-            pageTitle.Text = navItem.Title;
-        }
     }
 }
